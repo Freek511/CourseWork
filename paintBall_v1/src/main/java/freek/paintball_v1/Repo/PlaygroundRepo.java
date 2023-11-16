5396f38c3c1a5219bb65697facafdc163155cb14
Freek511 <feed03@uandex.ru>
2023-11-17 01:31:59 +0300

    feature: add new findByName method

--- paintBall_v1/src/main/java/freek/paintball_v1/Repo/PlaygroundRepo.java
+++ paintBall_v1/src/main/java/freek/paintball_v1/Repo/PlaygroundRepo.java
@@ -2,6 +2,8 @@ package freek.paintball_v1.Repo;
 
 import freek.paintball_v1.Entity.Playground;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface PlaygroundRepo extends JpaRepository<Playground, Integer> {
+    Optional <Playground> findByName(String name);
 }
