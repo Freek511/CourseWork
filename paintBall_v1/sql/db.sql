4c2cf869b4990e7250a5fc2fdd6b915c58fc6086
Артём <@LAPTOP-N8DISI0H>
2023-11-19 22:29:08 +0300

    refactor: update sql file

--- paintBall_v1/sql/db.sql
+++ paintBall_v1/sql/db.sql
@@ -1,32 +1,30 @@
-drop database if exists paintball;
-create database if not exists paintball;
-use paintball;
-
-CREATE TABLE IF NOT EXISTS Playground
-(
-    `pg_ID`             INT             NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `pg_Name`           VARCHAR(45)     NOT NULL,
-    `pg_Description`    VARCHAR(45)     NOT NULL,
-    `pg_Price`          INT             UNSIGNED NOT NULL,
-    `pg_Area`           INT             UNSIGNED NOT NULL,
-    `pg_Capacity`       INT             UNSIGNED NOT NULL
+create sequence _orders_seq start with 1 increment by 50;
+create sequence _playground_seq start with 1 increment by 50;
+create sequence _user_seq start with 1 increment by 50;
+create table _playground (
+                             area integer not null,
+                             capacity integer not null,
+                             id integer not null,
+                             price integer not null,
+                             description varchar(255),
+                             name varchar(255),
+                             primary key (id)
+);
+create table _user (
+                       id integer not null,
+                       email varchar(255),
+                       login varchar(255),
+                       password varchar(255),
+                       role varchar(255) check (role in ('ADMIN','USER')),
+                       primary key (id)
 );
 
-CREATE TABLE IF NOT EXISTS User
-(
-    `u_ID`       INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `u_Email`    VARCHAR(45)  NOT NULL UNIQUE,
-    `u_Login`    VARCHAR(45)  NOT NULL UNIQUE,
-    `u_Password` VARCHAR(100) NOT NULL,
-    `u_Role`     VARCHAR(10)  NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Orders
-(
-    `o_ID`             INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `o_Date`           VARCHAR(45) NOT NULL,
-    `user_ID`          INT         NOT NULL,
-    `playground_ID`    INT         NOT NULL,
-    FOREIGN KEY (`user_ID`) REFERENCES User (`u_ID`) ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY (`playground_ID`) REFERENCES Playground (`pg_ID`) ON DELETE RESTRICT ON UPDATE CASCADE
+create table _orders (
+                         id integer not null,
+                         playground_id integer,
+                         user_id integer,
+                         order_date varchar(255),
+                         primary key (id),
+                         foreign key (playground_id) references _playground (id),
+                         foreign key (user_id) references _user (id)
 );
