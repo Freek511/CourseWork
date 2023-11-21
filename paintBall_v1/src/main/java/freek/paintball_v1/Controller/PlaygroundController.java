4f3b0434d0ba4e0a0d2e0a851db49d3b7d7b1ad0
Артём <@LAPTOP-N8DISI0H>
2023-11-22 01:39:04 +0300

    refactor: move logic from controller to service

--- paintBall_v1/src/main/java/freek/paintball_v1/Controller/PlaygroundController.java
+++ paintBall_v1/src/main/java/freek/paintball_v1/Controller/PlaygroundController.java
@@ -2,69 +2,45 @@ package freek.paintball_v1.Controller;
 
 import freek.paintball_v1.DTO.PlaygroundUpdateRequest;
 import freek.paintball_v1.Entity.Playground;
-import freek.paintball_v1.Repo.PlaygroundRepo;
-import org.springframework.http.HttpStatusCode;
+import freek.paintball_v1.Service.PlaygroundService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/playgrounds")
+@RequiredArgsConstructor
 public class PlaygroundController {
-    private final PlaygroundRepo playgroundRepo;
+    private final PlaygroundService playgroundService;
 
-    public PlaygroundController(PlaygroundRepo playgroundRepo) {
-        this.playgroundRepo = playgroundRepo;
-    }
 
     @GetMapping("/all")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     public Iterable<Playground> getAllPlaygrounds(){
-        return playgroundRepo.findAll();
+        return playgroundService.getAllPlaygrounds();
     }
+
     @GetMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     public Object getPlaygroundById(@PathVariable int id){
-        if(playgroundRepo.findById(id).isEmpty()) {
-            return new ResponseEntity<>("Invalid id", HttpStatusCode.valueOf(400));
-        }
-        return playgroundRepo.findById(id).get();
+        return playgroundService.getPlaygroundById(id);
     }
-    @PostMapping
+    @PostMapping("/create")
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<String> createPlayground(@RequestBody Playground playground){
-        if(playgroundRepo.findByName(playground.getName()).isPresent()) {
-            return new ResponseEntity<>("Playground already exists", HttpStatusCode.valueOf(400));
-        }
-        playgroundRepo.saveAndFlush(playground);
-        return new ResponseEntity<>("Successfully added", HttpStatusCode.valueOf(201));
+       return playgroundService.createPlayground(playground);
     }
 
-    @PutMapping
+    @PutMapping("/update")
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<String> updatePlayground(@RequestBody PlaygroundUpdateRequest updateRequest){
-
-        if(!playgroundRepo.existsById(updateRequest.getId())) {
-            return new ResponseEntity<>("No such element in database", HttpStatusCode.valueOf(400));
-        }
-        Playground oldPlayground = playgroundRepo.getReferenceById(updateRequest.getId());
-        oldPlayground.setArea(updateRequest.getArea());
-        oldPlayground.setDescription(updateRequest.getDescription());
-        oldPlayground.setCapacity(updateRequest.getCapacity());
-        oldPlayground.setPrice(updateRequest.getPrice());
-
-        playgroundRepo.saveAndFlush(oldPlayground);
-        return new ResponseEntity<>("Successfully updated", HttpStatusCode.valueOf(200));
+        return playgroundService.updatePlayground(updateRequest);
     }
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<String> deletePlayground(@PathVariable int id){
-        if(!playgroundRepo.existsById(id))
-        {
-            return new ResponseEntity<>("No such element in database", HttpStatusCode.valueOf(400));
-        }
-        playgroundRepo.deleteById(id);
-        return new ResponseEntity<>("Successfully deleted", HttpStatusCode.valueOf(200));
+       return playgroundService.deletePlayground(id);
     }
 
 }
