073bb56585d9005ffd934ec32428f7df375de95b
Artem <feed03@yandex.ru>
2024-05-16 16:38:21 +0300

    Fix Email Service

--- paintBall_v1/src/main/java/freek/paintball_v1/Controller/MailController.java
+++ paintBall_v1/src/main/java/freek/paintball_v1/Controller/MailController.java
@@ -1,6 +1,7 @@
 package freek.paintball_v1.Controller;
 
 import freek.paintball_v1.Service.MyEmailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,17 +9,17 @@ import org.springframework.mail.MailException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/email")
 public class MailController {
 
-    @Autowired
-    MyEmailService emailService;
+    private final MyEmailService emailService;
 
-    @GetMapping(value = "/{user-email}")
-    public  ResponseEntity<String> sendSimpleEmail(@PathVariable("user-email") String email) {
+    @GetMapping("/{id}")
+    public  ResponseEntity<String> sendSimpleEmail(@PathVariable int id) {
 
         try {
-            emailService.sendSimpleEmail(email, "Welcome", "This is a welcome email for your!!");
+            emailService.sendSimpleEmail(id);
         } catch (MailException mailException) {
             return new ResponseEntity<>("Unable to send email", HttpStatus.INTERNAL_SERVER_ERROR);
         }
--- paintBall_v1/src/main/java/freek/paintball_v1/Service/MyEmailService.java
+++ paintBall_v1/src/main/java/freek/paintball_v1/Service/MyEmailService.java
@@ -1,20 +1,41 @@
 package freek.paintball_v1.Service;
-import org.springframework.beans.factory.annotation.Autowired;
+import freek.paintball_v1.DTO.OrderResponse;
+import freek.paintball_v1.Entity.User;
+import freek.paintball_v1.Repo.OrdersRepo;
+import freek.paintball_v1.Repo.PlaygroundRepo;
+import freek.paintball_v1.Repo.UserRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MyEmailService  {
-    @Autowired
-    public JavaMailSender mailSender;
 
-    public void sendSimpleEmail(String toAddress, String subject, String message) {
+    private final JavaMailSender mailSender;
+    private final OrdersRepo ordersService;
+    private final PlaygroundRepo playgroundRepo;
+    private final UserRepo userRepo;
+
+
+    public void sendSimpleEmail(int order_id)
+    {
+        var order = ordersService.getReferenceById(order_id);
+        var user = userRepo.getReferenceById(order.getUser().getId());
+        var playground = playgroundRepo.getReferenceById(order.getPlayground().getId());
+        String toAddress = user.getEmail();
+        String login = user.getLogin();
+
+        String message = "Hello " + login + "! Your order with id: " + order_id + " successfully accepted! " +
+                "We are waiting you on " + playground.getName() + " by " + order.getOrderDate();
+
 
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("point.r-123@yandex.ru");
+        simpleMailMessage.setFrom("pbtop.no.reply@yandex.ru");
         simpleMailMessage.setTo(toAddress);
-        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setSubject("Order was accepted!");
         simpleMailMessage.setText(message);
         mailSender.send(simpleMailMessage);
     }
